e43367bf7c2bb08650b8d1074e1bab478fe6e598
mrshine4k <111043449+mrshine4k@users.noreply.github.com>
2022-12-25 21:27:15 +0700

    Designed homepage

---
+++ ClassesForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class ClassesForm : Form
+    {
+        public ClassesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
